3b3c202e98b3c75d8ad04dafad9adb5fe3f30092
iskan-dar05 <skous2034@gmail.com>
2025-11-22 19:20:29 +0100

    first com

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.get("/", (req, res)=>{
+        res.json({message: "hello world"})
+})
+
+app.listen(PORT, ()=>{
+        console.log(`listen on port {PORT}`)
+})
