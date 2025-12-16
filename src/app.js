6cd03fd42f5524e283cb8f8d4a70d21cb5916ba1
iskandar boukhenoufa <iskandar@iskandar.iskandar@skou.com>
2025-12-16 05:40:48 -0500

    initialcommit

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import express from 'express'
 const app = express()
 
 app.get("/", (req, res)=>{
-	res.status(200).send("hello from Acquisitions"(
+	res.status(200).send("hello from Acquisitions")
 })
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-import app from './app'
+import app from './app.js'
 
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, ()=>{
 	console.log(`Server listening at http://localhost:${PORT}`)
-}
+})
